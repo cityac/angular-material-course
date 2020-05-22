8a8f8b8646fa5cabc04de4960852ec826df987d5
Stanislav Aituhanov <aytuganov@gmail.com>
2020-05-22 09:12:20 +0300

    Section 4: deep deive into material is done

--- fitness-tracker-project/src/app/app.module.ts
+++ fitness-tracker-project/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HeaderComponent } from './navigation/header/header.component';
 import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
+import { StopTraningComponent } from './training/current-training/stop-training.component';
 
 @NgModule({
   declarations: [
@@ -29,6 +30,7 @@ import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.com
     WelcomeComponent,
     HeaderComponent,
     SidenavListComponent,
+    StopTraningComponent,
   ],
   imports: [
     BrowserModule,
@@ -41,5 +43,6 @@ import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.com
   ],
   providers: [],
   bootstrap: [AppComponent],
+  entryComponents: [StopTraningComponent],
 })
 export class AppModule {}
--- fitness-tracker-project/src/app/training/current-training/current-training.component.ts
+++ fitness-tracker-project/src/app/training/current-training/current-training.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { StopTraningComponent } from './stop-training.component';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-current-training',
@@ -6,7 +9,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./current-training.component.scss'],
 })
 export class CurrentTrainingComponent implements OnInit {
-  constructor() {}
+  progress = 0;
+  timer;
+  @Output() trainingExit = new EventEmitter<void>();
 
-  ngOnInit(): void {}
+  constructor(private dialog: MatDialog) {}
+
+  ngOnInit(): void {
+    this.startOrResumeTraining();
+  }
+
+  startOrResumeTraining() {
+    this.timer = setInterval(() => {
+      this.progress = this.progress + 1;
+      if (this.progress >= 100) {
+        clearInterval(this.timer);
+      }
+    }, 200);
+  }
+
+  onStop() {
+    clearInterval(this.timer);
+    const dialogRef = this.dialog.open(StopTraningComponent, {
+      width: '300px',
+      data: { progress: this.progress },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.trainingExit.emit();
+      } else {
+        this.startOrResumeTraining();
+      }
+    });
+  }
 }
